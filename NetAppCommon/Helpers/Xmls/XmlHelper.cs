6f142fcff5fab4d7cf2fc615baa1ea3f8f6664cd
Piotr Paul <piotr.paul@gmail.com>
2020-10-26 23:30:39 +0100

    Dodanie klasy pomocnika XML

---
+++ NetAppCommon/Helpers/Xmls/XmlHelper.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace NetAppCommon.Helpers.Xmls
+{
+    /// <summary>
+    /// Klasa pomocnika XML
+    /// XML helper class
+    /// </summary>
+    public class XmlHelper
+    {
+
+        #region private static readonly log4net.ILog log4net
+        /// <summary>
+        /// Log4net Logger
+        /// Log4net Logger
+        /// </summary>
+        private static readonly log4net.ILog log4net = Log4netLogger.Log4netLogger.GetLog4netInstance(MethodBase.GetCurrentMethod().DeclaringType);
+        #endregion
+
+        #region
+        public static T DeserializeXmlFromFile<T>(string filePath)
+        {
+            try
+            {
+                if (null != filePath && !string.IsNullOrWhiteSpace(filePath) && File.Exists(filePath))
+                {
+                    return DeserializeXmlFromString<T>(File.ReadAllText(filePath));
+                }
+            }
+            catch (Exception e)
+            {
+                log4net.Error(string.Format("{0}, {1}", e.Message, e.StackTrace), e);
+            }
+            return (T)Convert.ChangeType(null, typeof(T));
+        }
+        #endregion
+
+        #region public static T DeserializeXml<T>(string xml)
+        /// <summary>
+        /// Ogólna metoda deserializacja ciągu xml do typu T
+        /// General method to deserialize the xml string to type T
+        /// </summary>
+        /// <typeparam name="T">
+        /// Typ danych T
+        /// T data type
+        /// </typeparam>
+        /// <param name="xml">
+        /// Ciąg xml jako string
+        /// The xml string as a string
+        /// </param>
+        /// <returns>
+        /// Objekt typu danych T
+        /// Object of the data type T
+        /// </returns>
+        public static T DeserializeXmlFromString<T>(string xml)
+        {
+            try
+            {
+                XmlReader xmlReader = XmlReader.Create(new StringReader(xml));
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+                T o = (T)xmlSerializer.Deserialize(xmlReader);
+                if (null != o)
+                {
+                    log4net.Debug(string.Format("{0} OK", o));
+                    return o;
+                }
+                log4net.Debug(string.Format("{0} EMPTY", o));
+            }
+            catch (Exception e)
+            {
+                log4net.Error(string.Format("{0}, {1}", e.Message, e.StackTrace), e);
+            }
+            return (T)Convert.ChangeType(null, typeof(T));
+        }
+        #endregion
+    }
+}
